53027d1278b126e141c4fdf642862ffd5601348f
yroslaw12252005 <81955354+yroslaw12252005@users.noreply.github.com>
2024-12-27 15:28:19 +0300

    V1

--- leads/models.py
+++ leads/models.py
@@ -5,7 +5,7 @@ class Record(models.Model):
     name = models.CharField(max_length=50,default=None)
     phone = models.CharField(max_length=50, default=None)
     description  = models.CharField(max_length=500, default=None)
-    status = models.CharField(max_length=50,  default=None)
+    status = models.CharField(max_length=50,  default='Новая')
     created_at = models.DateTimeField(auto_now_add=True)
     employees_KC = models.CharField(max_length=50, default="Не прикреплен")
     employees_UPP = models.CharField(max_length=50,default="Не прикреплен")
