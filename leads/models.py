cca8df4e47cbf916e3779382317f1f41e78ea4c8
yroslaw12252005 <81955354+yroslaw12252005@users.noreply.github.com>
2024-12-27 15:38:53 +0300

    V1

--- leads/models.py
+++ leads/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Record(models.Model):
-    name = models.CharField(max_length=50,default=None)
-    phone = models.CharField(max_length=50, default=None)
-    description  = models.CharField(max_length=500, default=None)
-    status = models.CharField(max_length=50,  default='Новая')
+    name = models.CharField(max_length=50,default=None, null=True)
+    phone = models.CharField(max_length=50, default=None, null=True)
+    description  = models.CharField(max_length=500, default=None, null=True)
+    status = models.CharField(max_length=50,  default='Новая', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     employees_KC = models.CharField(max_length=50, default="Не прикреплен")
     employees_UPP = models.CharField(max_length=50,default="Не прикреплен")
