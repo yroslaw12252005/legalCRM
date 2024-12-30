fcc16cd8f4e6923aad06c858ca6574334d3ad2ae
yroslaw12252005 <81955354+yroslaw12252005@users.noreply.github.com>
2024-12-30 13:55:23 +0300

    V1

--- coming/urls.py
+++ coming/urls.py
@@ -4,7 +4,7 @@ from coming.views import *
 
 urlpatterns = [
     path('', appointments, name='appointments'),
-    path('add_appointments/', add_appointment, name='add_appointments'),
+    path('add_appointments/<int:pk>/', add_appointment, name='add_appointments'),
     path("come_True/<int:pk>/", come_True, name='come_True'),
     path("come_False/<int:pk>/", come_False, name='come_False')
 ]
