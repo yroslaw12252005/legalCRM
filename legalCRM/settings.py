eac33b0df5a15819601e5881677602f58529a9b2
yroslaw12252005 <81955354+yroslaw12252005@users.noreply.github.com>
2025-01-13 10:14:32 +0300

    V1

--- legalCRM/settings.py
+++ legalCRM/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'legalCRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'default_db',
+		'HOST': '192.168.0.4',
+		'PORT': 3306,
+		'USER': 'gen_user',
+		'PASSWORD': 'T925BO750ru'
+	}
 }
 
 
