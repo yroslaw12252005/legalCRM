9753cc08fb73e06e658e0b806f541a7bebe30d28
yroslaw12252005 <81955354+yroslaw12252005@users.noreply.github.com>
2025-01-13 10:43:50 +0300

    V1

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    username = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
+
 # Create your models here.
