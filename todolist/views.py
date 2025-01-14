db0dbbe0f231acb1a3b326ab23bc269426362ac6
yroslaw12252005 <81955354+yroslaw12252005@users.noreply.github.com>
2025-01-14 13:38:45 +0300

    V1

--- todolist/views.py
+++ todolist/views.py
@@ -11,11 +11,13 @@ def todolist(request):
     return render(request, "todolist.html", {'todolist':todolist})
 
 
-def add_task(request, pk):
+def add_task(request):
     form = AddTaskForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
-            add_task = form.save()
+            add_task = form.save(commit=False)
+            add_task.user = request.user.id  # The logged-in user
+            add_task.save()
             messages.success(request, f"Задача {add_task.title} успешно создана")
             return redirect("todolist")
-    return render(request, "add_task.html", {"form": form, 'pk':pk})
+    return render(request, "add_task.html", {"form": form, 'pk':request.user.id})
