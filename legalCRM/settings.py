2c0c3fd8f28c21a1a68052f9f2108df8cfb30225
yroslav <yroslawkoshkin@gmail.com>
2025-02-14 20:53:30 +0300

    V2

--- legalCRM/settings.py
+++ legalCRM/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'legalCRM.urls'
