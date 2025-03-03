04f29f04b20457fb2aefc51f5c2bbc952beab346
yroslaw12252005 <81955354+yroslaw12252005@users.noreply.github.com>
2025-03-03 13:23:00 +0300

    V3

--- smart_calendar/forms.py
+++ smart_calendar/forms.py
@@ -7,5 +7,8 @@ class AddEventForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['client', 'start_time', 'end_time']
+        widgets = {
+            "name": forms.DateTimeInput(attrs={"id": "start-time", "label": "Your name"})
 
+        }
         labels = {'client': 'Клиент', "start_time":"Начло приема", "end_time":"Конец приема"}
