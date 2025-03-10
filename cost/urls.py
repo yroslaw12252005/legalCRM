0b5a3c7ae84525f2b8e6157a39590f7a6a585cfb
yroslav <yroslawkoshkin@gmail.com>
2025-03-10 23:04:30 +0300

    V3

--- cost/urls.py
+++ cost/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from cost import views
 urlpatterns = [
     path("/<int:pk>/", views.cost, name='cost'),
+    path("user_inform/<int:pk>/", views.percent, name='user_inform'),
 ]
--- cost/views.py
+++ cost/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.db.models import Sum
 
 from .forms import Surcharge_form
 
 from leads.models import Record
+from accounts.models import User
 
 def cost(request, pk):
     record = Record.objects.get(id=pk)
@@ -16,3 +18,14 @@ def cost(request, pk):
             messages.success(request, f"Доплата добавленна")
         return render(request, "surcharge.html",
                   {"surcharge_form": surcharge_form})
+
+def percent(request, pk):
+    cash = Record.objects.filter(employees_UPP=pk).aggregate(Sum('cost'))
+    percent_user = User.objects.get(id=pk)
+    zp = float(cash['cost__sum'])*float(percent_user.percent)
+    return render(request, "user_inform.html",
+                  {"zp": zp, "cash":cash})
+def bet(request, pk):
+    pass
+def percent_and_bet(request):
+    pass
