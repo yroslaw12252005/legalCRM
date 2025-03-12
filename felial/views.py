db1eca28fa87f9141ad4a2c9f4ae65de49e559a5
yroslav <yroslawkoshkin@gmail.com>
2025-03-12 23:53:14 +0300

    V3

--- felial/views.py
+++ felial/views.py
@@ -10,7 +10,7 @@ def add_felial(request):
     if request.user.is_authenticated:
         if form.is_valid():
             add_felial = form.save(commit=False)
-            add_felial.companys = request.user.companys  # The logged-in user
+            add_felial.companys = request.user.companys  # The loggjed-in user
             add_felial.save()
             messages.success(request, f"Фелиал {add_felial.title} успешно создана")
             return redirect("home")
