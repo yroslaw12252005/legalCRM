93f122f92f54d68c185b1906659576db8c2b5ec1
yroslaw12252005 <81955354+yroslaw12252005@users.noreply.github.com>
2025-03-20 18:58:18 +0300

    V3

--- smart_calendar/models.py
+++ smart_calendar/models.py
@@ -18,12 +18,7 @@ class Booking(models.Model):
         on_delete=models.SET_NULL,
         null=True
     )
-    registering = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        null=True,
-        related_name='registering'
-    )
+
     come = models.IntegerField(default=0)
     companys = models.ForeignKey(
         Companys,
