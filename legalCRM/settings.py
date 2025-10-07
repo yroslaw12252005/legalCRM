e17108cc61ebc9094e66d6d7efe95d4486a17d95
yroslav <yroslawkoshkin@gmail.com>
2025-10-07 23:48:49 +0300

    V6

--- legalCRM/settings.py
+++ legalCRM/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-%8itp5v^vn=edeh()b80@xroxy_uk=f&_mw@-41b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'true').lower() == 'true'
 
 ALLOWED_HOSTS = ["yblochko.rosscrm.ru",'127.0.0.1', '46.229.213.252', "78.40.217.164"]
 
